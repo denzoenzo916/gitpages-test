146721d6c0cd733b8d9b1245346ce294777ffc5b
denzoenzo916 <hoang.denny@gmail.com>
2025-05-10 17:07:27 -0700

    corrected docusaurus config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'denzoenzo916', // Usually your GitHub org/user name.
   projectName: 'gitpages-test', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
